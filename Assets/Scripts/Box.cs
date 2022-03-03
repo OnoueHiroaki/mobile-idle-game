889c2aac88afec83fb38b19ebec7d65dd8c28ef2
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-03-03 17:41:00 +0900

    収穫機能を追加しました。

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class Box : MonoBehaviour
 {
     public static Box Instance { get; private set; }
-    [SerializeField] GameObject _harvestArea1, _harvestArea2;
+    [SerializeField] Transform _harvestArea1, _harvestArea2;
     List<MaterialBase> _materials;
     public List<MaterialBase> Materials { get => _materials; set { } }
     private void Awake()
     {
         Instance = this;
+    }
+    private void Start()
+    {
+
+    }
+    public void GetMaterial()
+    {
+        var child1 = _harvestArea1;
+        var child2 = _harvestArea2;
+        for (int i = 0; i < child1.GetChildCount(); i++)
+        {
+            _materials.Add(child1.GetChild(i).gameObject.GetComponent<MaterialBase>());
+            Destroy(child2.GetChild(i).gameObject);
+        }
+        for (int i = 0; i < child2.GetChildCount(); i++)
+        {
+            _materials.Add(child2.GetChild(i).gameObject.GetComponent<MaterialBase>());
+            Destroy(child2.GetChild(i).gameObject);
+        }
     }
 }
