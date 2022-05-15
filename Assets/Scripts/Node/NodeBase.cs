5d3c6f2bf46b10fc46096a0dde809febc929d6e6
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-16 01:12:40 +0900

    ノードのスクリプトを追加

--- Assets/Scripts/Node/NodeBase.cs
+++ Assets/Scripts/Node/NodeBase.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public enum NodeType
 {
     Person,
-
 }
-public class Node : MonoBehaviour
+public class NodeBase : MonoBehaviour
 {
     //レシピで呼びまとめる
-    public Node(Node person, Node left, Node right, Node up, Node down, int level, int threshold, string label, string decision,bool nextPath)
+    public NodeBase(NodeBase person, int level, int threshold, string label, string decision, bool nextPath)
     {
         Person = person;
-        Left = left;
-        Right = right;
-        Up = up;
-        Down = down;
         Level = level;
         Threshold = threshold;
         Label = label;
@@ -24,11 +19,8 @@ public class Node : MonoBehaviour
         NextPath = nextPath;
     }
 
-    public Node Person; // 親ノード
-    public Node Left; // 左側の子ノード
-    public Node Right; // 右側の子ノード
-    public Node Up; // 上側のノード
-    public Node Down; // 下側のノード
+    public NodeBase Person; // 親ノード
+    public NodeBase Child; // 子ノード
     public int Level; // 階層
     public int Threshold; // しきい値
     public string Label; // ID
