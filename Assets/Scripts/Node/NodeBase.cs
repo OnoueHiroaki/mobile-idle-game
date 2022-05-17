d35789a0add5253cd9f948d43d28f7670777b39c
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-17 22:19:13 +0900

    IDと所持数の初期化手抜き

--- Assets/Scripts/Node/NodeBase.cs
+++ Assets/Scripts/Node/NodeBase.cs
@@ -21,6 +21,31 @@ public class NodeBase : MonoBehaviour
     [SerializeField] int _label;
     public int Label { get => _label; private set { } }
     //enumの種類
-    Enum[] types = new Enum[3];
+    Enum[] _types = new Enum[3];
+    //enumごとの要素数
+    List<int> _iDs = new List<int>();
+    //IDと所持数
     Dictionary<Enum, Dictionary<int, int>> Items = new Dictionary<Enum, Dictionary<int, int>>();
+    private void Start()
+    {
+        _types[0] = (GrassType)0;
+        _types[1] = (OreType)0;
+        _types[2] = (WaterType)0;
+        _iDs.Add();
+        //enumの数分回す
+        for (int i = 0; i < _types.Length; i++)
+        {
+            // アイテムのIDと所持数
+            var item = new Dictionary<int, int>();
+            
+            //enumのIDの数分回す
+            for (int k = 0; k < _iDs[i]; k++)
+            {
+                //アイテムの最初の所持数は0なので0を初期値として入れる
+                item.Add(k, 0);
+            }
+            //Dictionaryの初期化
+            Items.Add(_types[i], item);
+        }
+    }
 }
